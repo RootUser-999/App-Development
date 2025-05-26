776758ca0e9340e049bb1d30e0703b51a5239b8c
SHAHZEB <shahzabaman971@gmail.com>
2025-05-26 13:25:08 -0400

    new commit

--- flutter/flutterSecond/flutter_application_1/lib/main.dart
+++ flutter/flutterSecond/flutter_application_1/lib/main.dart
@@ -1,4 +1,25 @@
 
-void main(){
+import 'package:flutter/material.dart';
+
+void main()=> runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title:Text('My first app'
+      ),
+      centerTitle:true,
+      backgroundColor:Color(0xFF0000FF), // Blue color
+    ),
+    body:Center(
+      child:Text('hello world'),),
+    
+    floatingActionButton: FloatingActionButton(
+       
+      child:Text('click'),
+      onPressed: (){
+        print('Button clicked!');
+      },
+    ),
+  ),
+
+));
   
-}
